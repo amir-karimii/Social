51befd503c04243d65a709540d5b62f3477d7ecc
amirkarimi <amirkarimi@amirkarimis-MacBook-Air.local>
2025-07-25 13:20:11 +0330

    Social

--- A/account/views.py
+++ A/account/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.views import View
-from .forms import UserRegistrationForm, UserLoginForm
+from .forms import UserRegistrationForm, UserLoginForm, EditUserForm
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import views as auth_views
 from django.urls import reverse_lazy
-
+from .models import Relation
 
 class UserRegisterView(View):
     form_class = UserRegistrationForm
@@ -35,6 +35,10 @@ class UserLoginView(View):
     form_class = UserLoginForm
     template_name = 'account/login.html'
 
+    def setup(self, request, *args, **kwargs):
+        self.next = request.GET.get('next')
+        return super().setup(request, *args, **kwargs)
+
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
             return redirect('home:home')
@@ -52,6 +56,8 @@ class UserLoginView(View):
             if user is not None:
                 login(request, user)
                 messages.success(request, 'you logged in successfully', 'success')
+                if self.next:
+                    return redirect(self.next)
                 return redirect('home:home')
             messages.error(request, 'you login failed, please try again', 'warning')
         return render(request , self.template_name, {'form':form})
@@ -65,10 +71,17 @@ class UserLogoutView(LoginRequiredMixin,View):
 
 class UserProfileView(LoginRequiredMixin,View):
     def get(self, request, user_id):
+        is_following = False
         user = get_object_or_404(User,pk=user_id)
         posts = user.posts.all()
-        return render(request, 'account/profile.html', {'user':user, 'posts':posts})
-
+        relation = Relation.objects.filter(from_user=request.user, to_user=user)
+        if relation.exists():
+            is_following = True
+        return render(request, 'account/profile.html', {
+            'user': user,
+            'posts': posts,
+            'is_following': is_following
+        })
 
 class UserPasswordResetView(auth_views.PasswordResetView):
     template_name = 'account/password_reset_form.html'
@@ -85,3 +98,45 @@ class UserPasswordResetConfirmView(auth_views.PasswordResetConfirmView):
 
 class UserPasswordResetCompleteView(auth_views.PasswordResetCompleteView):
     template_name = 'account:password_reset_complete.html'
+
+
+class UserFollowView(LoginRequiredMixin, View):
+    def get(self, request, user_id):
+        user = User.objects.get(id=user_id)
+        relation = Relation.objects.filter(from_user=request.user, to_user=user)
+        if relation.exists():
+            messages.error(request, 'You are already following this user.', 'danger')
+        else:
+            Relation(from_user=request.user, to_user=user).save()
+            messages.success(request, 'You are now following this user.', 'success')
+        return redirect('account:user_profile', user.id)
+
+
+class UserUnfollowView(LoginRequiredMixin,View):
+    def get(self, request, user_id):
+        user = User.objects.get(id=user_id)
+        relation = Relation.objects.filter(from_user=request.user, to_user=user)
+        if relation.exists():
+            relation.delete()
+            messages.success(request, 'you unfollowing this user', 'success')
+        else:
+            messages.error(request, 'you are not following this user', 'danger')
+        return redirect('account:user_profile', user.id)
+
+
+
+class EditUserView(LoginRequiredMixin, View):
+    form_class = EditUserForm
+
+    def get(self, request):
+        form = self.form_class(instance=request.user.profile, initial={'email': request.user.email})
+        return render(request, 'account/edit_profile.html', {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            request.user.email = form.cleaned_data['email']
+            request.user.save()
+            messages.success(request, 'profile edited successfully', 'success')
+        return redirect('account:user_profile', request.user.id)
--- A/home/views.py
+++ A/home/views.py
@@ -1,24 +1,57 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.views import View
-from .models import Post
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
-from .forms import PostCreateUpdateView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils.decorators import method_decorator
 from django.utils.text import slugify
+from django.views import View
+
+from .forms import PostCreateUpdateView, CommentCreateForm, CommentReplyForm, PostSearchForm
+from .models import Post, Comment, Vote
+
 
 class HomeView(View):
+    form_class = PostSearchForm
+
     def get(self, request):
-        posts = Post.objects.all
-        return render(request, 'home/index.html', {'posts': posts})
+        posts = Post.objects.order_by('-created')
+        if request.GET.get('search'):
+            posts = posts.filter(body__contains=request.GET['search'])
+        return render(request, 'home/index.html', {'posts': posts, 'form': self.form_class})
+
 
 class PostDetailView(View):
-    def get(self, request, post_id, post_slug):
-        post = get_object_or_404(Post, id=post_id, slug=post_slug)
-        return render(request, 'home/detail.html', {'post': post})
+    form_class = CommentCreateForm
+    form_class_reply = CommentReplyForm
 
-class PostDeleteView(LoginRequiredMixin,View):
+    def setup(self, request, *args, **kwargs):
+        self.post_instance = get_object_or_404(Post, id=kwargs['post_id'], slug=kwargs['post_slug'])
+        return super().setup(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        comments = self.post_instance.pcomments.filter(is_reply=False)
+        can_like = False
+        if request.user.is_authenticated and self.post_instance.user_can_like(request.user):
+            can_like = True
+        return render(request, 'home/detail.html',
+                      {'post': self.post_instance, 'comments': comments, 'form': self.form_class,
+                       'reply_form': self.form_class_reply, 'can_like': can_like})
+
+    @method_decorator(login_required)
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.user = request.user
+            new_comment.post = self.post_instance
+            new_comment.save()
+            messages.success(request, 'your comment submitted successfully', 'success')
+            return redirect('home:post_detail', self.post_instance.id, self.post_instance.slug)
+
+
+class PostDeleteView(LoginRequiredMixin, View):
     def get(self, request, post_id):
-        post = get_object_or_404(Post,pk=post_id)
+        post = get_object_or_404(Post, pk=post_id)
         if post.user.id == request.user.id:
             post.delete()
             messages.success(request, 'Post deleted successfully', 'success')
@@ -27,11 +60,11 @@ class PostDeleteView(LoginRequiredMixin,View):
         return redirect('home:home')
 
 
-class PostUpdateView(LoginRequiredMixin,View):
+class PostUpdateView(LoginRequiredMixin, View):
     form_class = PostCreateUpdateView
 
     def setup(self, request, *args, **kwargs):
-        self.post_instance = get_object_or_404(Post,pk=kwargs['post_id'])
+        self.post_instance = get_object_or_404(Post, pk=kwargs['post_id'])
         return super().setup(request, *args, **kwargs)
 
     def dispatch(self, request, *args, **kwargs):
@@ -54,17 +87,17 @@ class PostUpdateView(LoginRequiredMixin,View):
             new_post.slug = slugify(form.cleaned_data['body'][:30])
             new_post.save()
             messages.success(request, 'you update this post', 'success')
-            return redirect('home:post-detail', post.id , post.slug)
+            return redirect('home:post_detail', post.id, post.slug)
 
 
-class PostCreateView(LoginRequiredMixin,View):
+class PostCreateView(LoginRequiredMixin, View):
     form_class = PostCreateUpdateView
 
-    def get(self,request,*args,**kwargs):
+    def get(self, request, *args, **kwargs):
         form = self.form_class
-        return render(request,'home/create.html',{'form':form})
+        return render(request, 'home/create.html', {'form': form})
 
-    def post(self,request,*args,**kwargs):
+    def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
         if form.is_valid():
             new_post = form.save(commit=False)
@@ -72,4 +105,34 @@ class PostCreateView(LoginRequiredMixin,View):
             new_post.user = request.user
             new_post.save()
             messages.success(request, 'you create a new post', 'success')
-            return redirect('home:post-detail', new_post.id , new_post.slug)
+            return redirect('home:post_detail', new_post.id, new_post.slug)
+
+
+class PostAddReplyView(LoginRequiredMixin, View):
+    form_class = CommentCreateForm
+
+    def post(self, request, post_id, comment_id):
+        post = get_object_or_404(Post, pk=post_id)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            reply = form.save(commit=False)
+            reply.user = request.user
+            reply.post = post
+            reply.reply = comment
+            reply.is_reply = True
+            reply.save()
+            messages.success(request, 'your reply submitted successfully', 'success')
+        return redirect('home:post_detail', post.id, post.slug)
+
+
+class PostLikeView(LoginRequiredMixin, View):
+    def get(self, request, post_id):
+        post = get_object_or_404(Post, id=post_id)
+        like = Vote.objects.filter(post=post, user=request.user)
+        if like.exists():
+            messages.error(request, 'you have already liked this post', 'danger')
+        else:
+            Vote.objects.create(post=post, user=request.user)
+            messages.success(request, 'you liked this post', 'success')
+        return redirect('home:post_detail', post.id, post.slug)
